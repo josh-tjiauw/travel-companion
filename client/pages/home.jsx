3c9ac13437928f65268d34d6ae9da4df72b39333
Josh Tjiauw <freezingmist2@gmail.com>
2021-02-01 16:45:09 -0800

    Added homepage with the title, and the 3 navigation buttons

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default function Home(props) {
   return (
-    <>
-    </>
+    <div className="home-container">
+    <h1 className="app-logo-text">Travel Companion</h1>
+
+    <button className="nav-btn">SEARCH A CITY</button>
+    <button className="nav-btn">VIEW YOUR ENTRIES</button>
+    <button className="nav-btn">VIEW TO-DO&apos;S</button>
+    </div>
   );
 }
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,59 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"firstName" TEXT NOT NULL,
+	"lastName" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "todos" (
+	"todoTitle" TEXT NOT NULL,
+	"isCompleted" BOOLEAN NOT NULL,
+	"todosCityId" integer NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "posts" (
+	"postId" serial NOT NULL,
+	"todosCityId" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	"imageLink" TEXT NOT NULL,
+	"createdBy" integer NOT NULL,
+	CONSTRAINT "posts_pk" PRIMARY KEY ("postId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "todosCity" (
+	"cityName" TEXT NOT NULL,
+	"createdBy" integer NOT NULL,
+	"isCompleted" BOOLEAN NOT NULL,
+	"todosCityId" serial NOT NULL,
+	CONSTRAINT "todosCity_pk" PRIMARY KEY ("todosCityId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "todos" ADD CONSTRAINT "todos_fk0" FOREIGN KEY ("todosCityId") REFERENCES "todosCity"("todosCityId");
+
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("todosCityId") REFERENCES "todosCity"("todosCityId");
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk1" FOREIGN KEY ("createdBy") REFERENCES "users"("userId");
+
+ALTER TABLE "todosCity" ADD CONSTRAINT "todosCity_fk0" FOREIGN KEY ("createdBy") REFERENCES "users"("userId");
