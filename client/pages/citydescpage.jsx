e5d93d70cc68d07a2d0a4c6e150fe81099250fa5
Josh Tjiauw <freezingmist2@gmail.com>
2021-02-09 12:55:37 -0800

    Fixed css styling

--- client/pages/citydescpage.jsx
+++ client/pages/citydescpage.jsx
@@ -7,8 +7,11 @@ export default function CityDescription(props) {
       <div>
         <PageTitle value={props.name} />
       </div>
-      <h2 className="text">Have you been here?</h2>
+      <h2 className="text" style={{ height: "50px" }}>
+        Have you been here?
+      </h2>
       <button className="nav-btn">Create a Review!</button>
+      <button className="nav-btn">View Other Reviews</button>
     </div>
   );
 }
